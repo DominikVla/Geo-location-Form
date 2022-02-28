0fdadbaa1436d7e76afc9eb47841f831e3a14cad
Dominik <74500880+DominikVla@users.noreply.github.com>
2022-02-28 13:58:34 +0000

    Add files via upload

--- Form/form.php
+++ Form/form.php
@@ -4,10 +4,21 @@
 $name = $_POST['Name']; // Name of person making the report
 $resources = $_POST['Resources']; // Resources they require
 $disaster = $_POST['Disaster']; // The type of disaster they're experiencing
+$urgency = $_POST['urgency']; // How urgent the issue is
+
+// Basic Data Input sanitisation
+$name = htmlspecialchars($name);
+$resources = htmlspecialchars($resources);
+$disaster = htmlspecialchars($disaster);
+$urgency = htmlspecialchars($urgency);
 
 // Location Data
 $lat = $_POST['Lat']; // Latitude
 $long = $_POST['Long']; // Logitude
+
+// Location Data Input Sanitisation
+$lat = htmlspecialchars($lat);
+$long = htmlspecialchars($long);
 
 
 // Database login details
@@ -18,8 +29,8 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO issuesrecorded (Name, Resources, Disaster, Latitude, Longitude)
-  VALUES ('$name', '$resources', '$disaster', '$lat', '$long')";
+  $sql = "INSERT INTO issuesrecorded (Name, Resources, Disaster, Latitude, Longitude, Urgency)
+  VALUES ('$name', '$resources', '$disaster', '$lat', '$long', '$urgency')";
   // use exec() because no results are returned
   $conn->exec($sql);
   echo "New record created successfully";
